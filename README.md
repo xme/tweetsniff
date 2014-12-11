a01d9981446a1dc9cb5bfe76a42a59642db7a80d
Xavier Mertens <xavier@rootshell.be>
2014-12-11 22:17:10 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+tweetsniff.py
+=============
+Grabs a user's Twitter feed via the Twitter API for local processing
+(storing to ElasticSearch, regex highlighting, etc)
+
+Installation
+------------
+The following Python modules are required:
+- twitter
+- elasticsearch
+- termcolor
+
+Install them with: pip install <module>
+
+Valid Twitter API key & token are required, see https://apps.twitter.com/
+
+Usage
+-----
+usage: tweetsniff.py [-h] [-c CONFIG]
+
+Display a Tweet feed
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        configuration file (default: /etc/tweetsniff.conf)
+
+Todo
+----
---
+++ tweetsniff.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+import argparse
+import errno
+import ConfigParser
+import json
+import os
+import re
+import signal
+import sys
+import syslog
+import time
+import twitter
+from datetime import datetime
+from dateutil import parser
+from elasticsearch import Elasticsearch
+from termcolor import colored
+
+api = None
+regex = None
+
+# Default configuration 
+config = { 'statusFile': '/var/run/tweetsniff.status' }
+
+def sigHandler(s, f):
+
+	"""Cleanup once CTRL-C is received"""
+
+	print "Killed."
+	sys.exit(0)
+
+def writeLog(msg):
+        syslog.openlog(logoption=syslog.LOG_PID,facility=syslog.LOG_MAIL)
+        syslog.syslog(msg)
+        return
+
+def indexEs(tweet):
+
+	"""Index a new Tweet in Elasticsearch"""
+
+	doc = tweet.AsDict()
+	# Delete 'retweeted_status' - to be fixed later
+	if 'retweeted_status' in doc:
+		del doc['retweeted_status']
+	# Delete 'urls' - to be fixed later?
+	if 'urls' in doc:
+		del doc['urls']
+
+	# To fix: support different timezones? (+00:00
+	doc['@timestamp'] = parser.parse(doc['created_at']).strftime("%Y-%m-%dT%H:%M:%S+00:00")
+	res = es.index(index=esIndex, doc_type='tweet', body=doc)
+	return
+
+def updateTimeline(first_id):
+
+	"""Get new Tweets from twitter.com"""
+
+	try:
+		timeline = api.GetHomeTimeline(since_id=first_id)
+	except twitter.error.TwitterError as e:
+		print "[Error] Twitter returned: %s (%d)" % (e[0][0]['message'], e[0][0]['code'])
+		return first_id
+
+	if not timeline:
+		return first_id
+
+	last_id = 0
+	for t in reversed(timeline):
+		text = t.text
+		for r in regex:
+			if r:
+				if re.search('('+r+')', text, re.I):
+					text = text.replace(r, colored(r, highlightColor))
+
+		print "%s | %15s | %s" % (parser.parse(t.created_at).strftime("%H:%M:%S"),
+					t.user.screen_name,
+					text)
+		if es:
+			indexEs(t)
+		if (t.id > last_id):
+			last_id = t.id
+	return(last_id)
+	
+def main():
+	global api
+	global config
+	global es
+	global regex
+	global highlightColor
+	global esIndex
+
+	signal.signal(signal.SIGINT, sigHandler)
+
+	parser = argparse.ArgumentParser(
+		description='Display a Tweet feed')
+	parser.add_argument('-c', '--config',
+		dest = 'configFile',
+		help = 'configuration file (default: /etc/tweetsniff.conf)',
+		metavar = 'CONFIG')
+	args = parser.parse_args()
+
+	if not args.configFile:
+		args.configFile = '/etc/tweetsniff.conf'
+
+	try:
+		c = ConfigParser.ConfigParser()
+		c.read(args.configFile)
+		# Twitter config
+		consumerKey = c.get('twitterapi', 'consumer_key')
+		consumerSecret = c.get('twitterapi', 'consumer_secret')
+		accessTokenKey = c.get('twitterapi', 'access_token_key')
+		accessTokenSecret = c.get('twitterapi', 'access_token_secret')
+		config['statusFile'] = c.get('twitterapi', 'status_file')
+		#Highligts
+		highlightColor = c.get('highlight', 'color')
+		highlightRegex = c.get('highlight', 'regex')
+		# Elasticsearch config (optional)
+		esServer = c.get('elasticsearch', 'server')
+		esIndex = c.get('elasticsearch', 'index')
+	except OSError as e:
+		writeLog('Cannot read config file %s: %s' % (args.configFile, e.errno()))
+		exit
+
+	print "DEBUG: %s, %s, %s, %s" % (consumerKey,consumerSecret,accessTokenKey,accessTokenSecret)
+	print "DEBUG: Regex: %s" % highlightRegex
+
+	if highlightRegex:
+		regex = highlightRegex.split('\n')
+
+	try:
+		api = twitter.Api(consumer_key = consumerKey,
+			consumer_secret = consumerSecret,
+			access_token_key = accessTokenKey,
+			access_token_secret = accessTokenSecret)
+	except:
+		print "[Error] Can't connect to twitter.com" 
+		sys.exit(1)
+
+	if esServer:
+		try:
+			es = Elasticsearch(
+				[esServer]
+				)
+		except:
+			print "[Warning] Can't connect to %s" % esServer
+
+
+	if not os.path.isfile(config['statusFile']):
+		print "DEBUG: Status file not found, starting new feed"
+		first_id = 0
+	else:
+		fd = open(config['statusFile'], 'r')
+                first_id = fd.read()
+                fd.close()
+		print "DEBUG: Restarting feed from ID %s" % first_id
+
+	while 1:
+		first_id = updateTimeline(first_id)
+		fd = open(config['statusFile'], 'w')
+		fd.write(str(first_id))
+		fd.close()
+		time.sleep(65)
+
+if __name__ == '__main__':
+	main()
